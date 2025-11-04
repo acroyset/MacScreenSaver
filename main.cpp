ec59f82f583e1c7ab6c78a915f1f7ee0bf132268
Andreas Royset <acroyset@gmail.com>
2025-11-03 19:39:32 -0800

    faster

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include <iostream>
 #include <fstream>
 #include <sstream>
+#define GLAD_GL_IMPLEMENTATION
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 #include <glm/glm.hpp>
@@ -99,13 +100,10 @@ static bool init() {
         return false;
     }
 
-    // OpenGL 3.3 core for macOS
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-    #ifdef __APPLE__
     glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GLFW_TRUE);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-    #endif
     glfwWindowHint(GLFW_REFRESH_RATE, 60);
 
     // Fullscreen on primary monitor
@@ -159,27 +157,106 @@ static void shutdown() {
     glfwTerminate();
 }
 
+float smoothstepCubed(float a, float b, float t)  {
+    float v = -2*t*t*t + 3*t*t;
+    return glm::mix(a, b, v);
+}
+
+float randomValue(glm::uint& state){
+    state = state * 747796405u + 2891336453u;
+    glm::uint result = ((state >> ((state >> 28) + 4u)) ^ state) * 277803737u;
+    result = (result >> 22) ^ result;
+    return float(result) * (1/4294967295.0);
+}
+
+float perlinNoise(glm::vec3 pos, int scale, glm::uvec3 res, glm::uint state){
+    glm::ivec3 base = glm::ivec3(pos/float(scale))*scale;
+    glm::vec3 frac = (pos - glm::vec3(base))/float(scale);
+
+    float mags[8] = {0,0,0,0,0,0,0,0};
+
+    for (int z = 0; z <= scale; z += scale){
+        for (int y = 0; y <= scale; y += scale){
+            for (int x = 0; x <= scale; x += scale){
+                int index = z/scale*4 + y/scale*2 + x/scale;
+                glm::uvec3 corner(base.x+x, base.y+y, base.z+z);
+                glm::uvec3 stateCorner(
+                    corner.x < res.x ? corner.x : corner.x-res.x,
+                    corner.y < res.y ? corner.y : corner.y-res.y,
+                    corner.z < res.z ? corner.z : corner.z-res.z
+                );
+                glm::uint stateTemp = glm::uint(stateCorner.z + stateCorner.y*res.x + stateCorner.x) + state;
+                glm::vec3 vector = glm::normalize(glm::vec3(2*randomValue(stateTemp)-1, 2*randomValue(stateTemp)-1, 2*randomValue(stateTemp)-1));
+                float mag = glm::dot(vector, (glm::vec3(corner)-pos)/float(scale));
+                mags[index] = (mag+1)/2;
+            }
+        }
+    }
+
+    for (int pass = 1; pass <= 3; pass ++){
+        for (int i = 0; i < 8/pow(2, pass-1); i += 2){
+            mags[i/2] = smoothstepCubed(mags[i], mags[i+1], frac[pass-1]);
+        }
+    }
+
+    return mags[0];
+}
+
+void updateNoiseMap(std::vector<glm::vec4>& noiseMap, const int scale, const float speed, const glm::uvec2 resolution, const float iTime) {
+    noiseMap.clear();
+    for (int y = 0; y < resolution.y; y += scale) {
+        for (int x = 0; x < resolution.x; x += scale) {
+            glm::ivec2 a = glm::ivec2(x, y);
+
+            glm::vec4 noise = glm::vec4(
+                perlinNoise(glm::vec3(a, int(iTime*speed)), 800, glm::uvec3(resolution, 252000), 0u),
+                perlinNoise(glm::vec3(a, int(iTime*speed)), 700, glm::uvec3(resolution, 252000), 1u),
+                perlinNoise(glm::vec3(a, int(iTime*speed)), 1000, glm::uvec3(resolution, 252000), 2u),
+                perlinNoise(glm::vec3(a, int(iTime*speed)), 900, glm::uvec3(resolution, 252000), 3u)
+                );
+
+            noiseMap.push_back(noise);
+        }
+    }
+}
+
 // ---------- Main ----------
 int main() {
     if (!init()) return -1;
 
-    glm::vec2 testVec = glm::vec2(1, 0);
-
     // Cache uniform locations
     const GLint uResolution = glGetUniformLocation(shaderProgram, "resolution");
-    const GLint uTestVec = glGetUniformLocation(shaderProgram, "testVec");
     const GLint uTime       = glGetUniformLocation(shaderProgram, "iTime");
+
+    int scale = 40;
+    float speed = 200;
+
+    glm::uvec2 resolution = glm::uvec2(fbWidth, fbHeight);
+
+    std::vector<glm::vec4> noiseMap;
+    noiseMap.resize(resolution.x*resolution.y/scale/scale);
+
+    GLuint ssboNoiseMap;
+    glGenBuffers(1, &ssboNoiseMap);
+    glBindBuffer(GL_SHADER_STORAGE_BUFFER, ssboNoiseMap);
+    glBufferData(GL_SHADER_STORAGE_BUFFER, int(noiseMap.size() * sizeof(glm::vec4)), noiseMap.data(), GL_STATIC_DRAW);
+    glBindBufferBase(GL_SHADER_STORAGE_BUFFER, 0, ssboNoiseMap);
 
     while (!glfwWindowShouldClose(window)) {
         // Update time
         const float t = static_cast<float>(glfwGetTime() - startTime);
-        testVec = glm::vec2(cos(t), sin(t));
 
         glClear(GL_COLOR_BUFFER_BIT);
 
+        updateNoiseMap(noiseMap, scale, speed, resolution, t);
+
+        glBindBuffer(GL_SHADER_STORAGE_BUFFER, ssboNoiseMap);
+        void* ptr = glMapBufferRange(GL_SHADER_STORAGE_BUFFER, 0, sizeof(glm::vec4)*noiseMap.size(),GL_MAP_WRITE_BIT | GL_MAP_INVALIDATE_BUFFER_BIT);
+        memcpy(ptr, noiseMap.data(), sizeof(glm::vec4)*noiseMap.size());
+        glUnmapBuffer(GL_SHADER_STORAGE_BUFFER);
+
         glUseProgram(shaderProgram);
         glUniform2ui(uResolution, static_cast<float>(fbWidth), static_cast<float>(fbHeight));
-        glUniform2f(uTestVec, testVec.x, testVec.y);
         glUniform1f(uTime, t);
 
         // Draw 1 big triangle via gl_VertexID (0..2) — no VBO needed
--- shaders/fullscreen.frag
+++ shaders/fullscreen.frag
@@ -1,56 +1,12 @@
-#version 330 core
+#version 430 core
 
 in vec2 fragCoord;
 out vec4 FragColor;
 
+layout(std430, binding = 0) buffer ssboNoiseMap { vec4 noiseMap[]; };
+
 uniform uvec2 resolution; // framebuffer size in pixels
-uniform vec2 testVec;
 uniform float iTime;      // seconds since start
-
-float smoothstepCubed(float a, float b, float t)  {
-    float v = -2*t*t*t + 3*t*t;
-    return mix(a, b, v);
-}
-
-float randomValue(inout uint state){
-    state = state * 747796405u + 2891336453u;
-    uint result = ((state >> ((state >> 28) + 4u)) ^ state) * 277803737u;
-    result = (result >> 22) ^ result;
-    return float(result) * (1/4294967295.0);
-}
-
-float perlinNoise(vec3 pos, int scale, uvec3 res, uint state){
-    ivec3 base = ivec3(pos/scale)*scale;
-    vec3 frac = (pos - vec3(base))/scale;
-
-    float mags[8] = float[8](0,0,0,0,0,0,0,0);
-
-    for (int z = 0; z <= scale; z += scale){
-        for (int y = 0; y <= scale; y += scale){
-            for (int x = 0; x <= scale; x += scale){
-                int index = z/scale*4 + y/scale*2 + x/scale;
-                vec3 corner = vec3(base.x+x, base.y+y, base.z+z);
-                vec3 stateCorner = vec3(
-                    corner.x < res.x ? corner.x : corner.x-res.x,
-                    corner.y < res.y ? corner.y : corner.y-res.y,
-                    corner.z < res.z ? corner.z : corner.z-res.z
-                );
-                uint stateTemp = uint(stateCorner.z + stateCorner.y*res.x + stateCorner.x) + state;
-                vec3 vector = normalize(vec3(2*randomValue(stateTemp)-1, 2*randomValue(stateTemp)-1, 2*randomValue(stateTemp)-1));
-                float mag = dot(vector, (vec3(corner)-pos)/scale);
-                mags[index] = (mag+1)/2;
-            }
-        }
-    }
-
-    for (int pass = 1; pass <= 3; pass ++){
-        for (int i = 0; i < 8/pow(2, pass-1); i += 2){
-            mags[i/2] = smoothstepCubed(mags[i], mags[i+1], frac[pass-1]);
-        }
-    }
-
-    return mags[0];
-}
 
 float distToSegment(vec2 p, vec2 a, vec2 b) {
     vec2 ab = b - a;
@@ -90,10 +46,11 @@ vec3 hsb2rgb(vec3 c) {
 // Simple animated gradient; replace with your effect.
 void main(){
     // Optional: aspect-corrected UV if you need square pixels
-    ivec2 screenSpace = ivec2(int(fragCoord.x*resolution.x), int(fragCoord.y*resolution.y));
+    ivec2 screenSpace = ivec2(int(fragCoord.x*resolution.x), int(fragCoord.y*resolution.y)) - ivec2(0, 20);
     vec2 uv = fragCoord * vec2(float(resolution.x)/float(resolution.y), 1.0);
 
     int scale = 40;
+    float speed = 200;
 
     vec4 accum = vec4(0);
 
@@ -104,6 +61,8 @@ void main(){
 
     if (minPos.x < 0) minPos.x = 0;
     if (minPos.y < 0) minPos.y = 0;
+    if (maxPos.x >= resolution.x) maxPos.x = int(resolution.x) - 1;
+    if (maxPos.y >= resolution.y) maxPos.y = int(resolution.y) - 1;
 
     minPos = minPos/scale*scale;
     maxPos = (maxPos/scale + ivec2(1))*scale;
@@ -112,18 +71,17 @@ void main(){
         for (int y = minPos.y; y <= maxPos.y; y += scale){
             ivec2 a = ivec2(x, y);
 
-            vec2 HS = vec2(
-                perlinNoise(vec3(a, int(iTime*200)), 800, uvec3(resolution, 252000), 0u),
-                perlinNoise(vec3(a, int(iTime*200)), 700, uvec3(resolution, 252000), 1u)
-            );
-            float mag = 2*perlinNoise(vec3(a, int(iTime*200)), 1000, uvec3(resolution, 252000), 3u);
+            uint index = y/scale * resolution.x/scale + x/scale - 1;
+
+            vec2 HS = noiseMap[index].xy;
+            float mag = 2*noiseMap[index].z;
             mag = mag*mag / 2;
 
             vec3 color = hsb2rgb(vec3(HS, mag));
 
             color *= 2.5*mag;
 
-            float angle = 2*3.14159265*perlinNoise(vec3(a, int(iTime*200)), 900, uvec3(resolution, 252000), 4u);
+            float angle = 2*3.14159265*noiseMap[index].w;
             vec2 vector = vec2(cos(angle), sin(angle));
 
             ivec2 b = a+ivec2(vector*scale*4*mag);
